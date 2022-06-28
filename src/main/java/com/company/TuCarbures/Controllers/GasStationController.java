fc79babd445e9c509ea1feb39ee36811ebfa35ea
lise6262 <75783788+lise6262@users.noreply.github.com>
2022-06-28 15:43:05 +0200

    function api

--- src/main/java/com/company/TuCarbures/Controllers/GasStationController.java
+++ src/main/java/com/company/TuCarbures/Controllers/GasStationController.java
@@ -1,21 +1,18 @@
 package com.company.TuCarbures.Controllers;
-import com.company.TuCarbures.ApiError;
+
 import com.company.TuCarbures.ApiErrors;
 import com.company.TuCarbures.Classes.Fuel;
-import io.swagger.v3.oas.annotations.Operation;
 import com.company.TuCarbures.Classes.GasStation;
 import com.company.TuCarbures.Repositories.GasStationRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import lombok.extern.slf4j.Slf4j;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.springframework.util.ObjectUtils.isEmpty;
+import java.util.HashMap;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/StationService")
@@ -29,6 +26,24 @@ public class GasStationController {
 
     private ApiErrors apiErrors = new ApiErrors();
 
+    //Fournir le dernier relevé de prix de chaque carburant disponible avec la date du
+    //relevé pour une station service donnée (pour l’affichage de la station favorite)
+
+    @GetMapping(value = "/Releve/{id}")
+    @Operation(summary = "relevé de prix de chaque carburant pour une station")
+    public HashMap<String, Double> priceOfFuels(@PathVariable("id") String id){
+        GasStation gasStation = gasStationRepository.findById(id).get();
+
+        HashMap<String, Double> fuelPrice = new HashMap<String,Double>();
+        for (Fuel fuel : gasStation.getFuels()) {
+            String fuelName = fuel.getFuelName();
+            Double price = fuel.getPrice();
+            fuelPrice.put(fuelName,price);
+        }
+
+        return fuelPrice;
+    }
+
     @PostMapping(value = "/StationService")
     @Operation(summary = "ajouter une station, avec les carburants : Sans Plomb 98 (E5)\n" +
             "- Sans Plomb 95 (E5)\n" +
@@ -38,17 +53,13 @@ public class GasStationController {
             "- GPL ")
     public ResponseEntity<String> postGasStation(@RequestBody GasStation gasStation) {
 
-        if(gasStation.gasStationName.equals("string")) {
+        if(gasStation.gasStationName.equals("string") || gasStation.id.equals("string")) {
             apiErrors.addError("404", "la station est vide");
-            return ResponseEntity.badRequest().body("la valeur de l'id n'est pas correct");
+            return ResponseEntity.badRequest().body("la station n'est pas correct");
         }
-//        List<Fuel> fuelList = gasStation.fuels;
-//        if() {
-//            apiErrors.addError("404", "le carburant n'est pas correct");
-//            return ResponseEntity.badRequest().body("la valeur de l'id du carburant n'est pas correct");
-//        }
         gasStationRepository.save(gasStation);
 
        return ResponseEntity.ok(gasStation.id);
     }
+
 }
