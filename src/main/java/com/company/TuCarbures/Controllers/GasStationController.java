c13cad5d891e8c825b7d1bf65ab36e1d724a6c38
Dimitri Degroise <68024482+DimitriAFC@users.noreply.github.com>
2022-06-29 15:18:42 +0200

    Add station name and station brand

--- src/main/java/com/company/TuCarbures/Controllers/GasStationController.java
+++ src/main/java/com/company/TuCarbures/Controllers/GasStationController.java
@@ -2,10 +2,7 @@ package com.company.TuCarbures.Controllers;
 
 
 import com.company.TuCarbures.ApiErrors;
-
 import com.company.TuCarbures.Classes.Fuel;
-import io.swagger.v3.oas.annotations.Operation;
-
 import com.company.TuCarbures.Classes.GasStation;
 import com.company.TuCarbures.Repositories.GasStationRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,22 +10,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/StationService")
@@ -47,21 +34,21 @@ public class GasStationController {
 
     @GetMapping(value = "/Releve/{id}")
     @Operation(summary = "relevé de prix de chaque carburant pour une station")
-    public HashMap<String, Double> priceOfFuels(@PathVariable("id") String id){
+    public HashMap<String, Double> priceOfFuels(@PathVariable("id") String id) {
         Optional<GasStation> station = gasStationRepository.findById(id);
 
-        HashMap<String, Double> fuelPrice = new HashMap<String,Double>();
+        HashMap<String, Double> fuelPrice = new HashMap<String, Double>();
         for (Fuel fuel : station.get().getFuels()) {
             String fuelName = fuel.getFuelName();
             Double price = fuel.getPrice();
 
-            fuelPrice.put(fuelName,price);
+            fuelPrice.put(fuelName, price);
         }
 
         return fuelPrice;
     }
 
-    @PostMapping(value = "/StationService")
+    @PostMapping(value = "/")
     @Operation(summary = "ajouter une station, avec les carburants : Sans Plomb 98 (E5)\n" +
             "- Sans Plomb 95 (E5)\n" +
             "- Sans Plomb 95 (E10)\n" +
@@ -70,20 +57,34 @@ public class GasStationController {
             "- GPL ")
     public ResponseEntity<String> postGasStation(@RequestBody GasStation gasStation) {
 
-        if(gasStation.gasStationName.equals("string") || gasStation.id.equals("string")) {
+        if (gasStation.gasStationName.equals("string") || gasStation.id.equals("string")) {
             apiErrors.addError("404", "la station est vide");
             return ResponseEntity.badRequest().body("la station n'est pas correct");
         }
         gasStationRepository.save(gasStation);
 
-       return ResponseEntity.ok(gasStation.id);
+        return ResponseEntity.ok(gasStation.id);
     }
 
 
-    @GetMapping("StationService/{id}")
+    @GetMapping("/{id}")
     @Operation(summary = "Retourner une station avec ses carburants")
     public Optional<GasStation> getStation(@PathVariable("id") String id) {
         return gasStationRepository.findById(id);
     }
 
+    @GetMapping("/marque")
+    @Operation(summary = "Les marques de stations service")
+    public HashMap<String, String> brandOfStation() {
+        Iterable<GasStation> station = gasStationRepository.findAll();
+        HashMap<String, String> brandGas = new HashMap<String, String>();
+        for (GasStation gasStation : station) {
+            String stationName = gasStation.gasStationName;
+            String brandName = gasStation.brand;
+
+            brandGas.put(stationName, brandName);
+        }
+
+        return brandGas;
+    }
 }
