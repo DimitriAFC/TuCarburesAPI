0bd6c69987e3b86bbd78c45296c3510ba4b710d8
lise6262 <75783788+lise6262@users.noreply.github.com>
2022-06-30 08:42:40 +0200

    modify connexion

--- src/main/java/com/company/TuCarbures/Controllers/UserController.java
+++ src/main/java/com/company/TuCarbures/Controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/User")
@@ -30,34 +31,21 @@ public class UserController {
 
     @GetMapping(path = "/connexion/{userName}/{password}")
     @Operation(summary = "connexion de l'utilisateur")
-    public User connexion(@PathVariable("userName") String userName, @PathVariable("password") String password) {
+    public Optional<User> connexion(@PathVariable("userName") String userName, @PathVariable("password") String password) {
         Iterable<User> users = userRepository.findAll();
-
-        User resultFinal = null;
         List<User> result = new ArrayList<>();
         users.forEach(result::add);
+        Optional<User> user = null;
 
         for (int i = 0; i < result.size(); i++) {
             String userNameList = result.get(i).userName;
             String passwordList = result.get(i).password;
             String id = result.get(i).id;
-            String adress = result.get(i).adress;
-            String zipCode = result.get(i).zipCode;
-            String city = result.get(i).city;
-            String email = result.get(i).email;
-            String favoriteFuel = result.get(i).favoriteFuel;
-            String favoriteStation = result.get(i).favoriteStation;
-            String numberPhone = result.get(i).numberPhone;
 
             if (userName.equals(userNameList) && password.equals(passwordList)) {
-                resultFinal = new User(id, userNameList, favoriteStation, favoriteFuel,
-                        adress, zipCode, city, numberPhone, passwordList, email);
-                return resultFinal;
-            } else {
-                return null;
+                user = userRepository.findById(id);
             }
-
         }
-        return resultFinal;
+        return user;
     }
 }
